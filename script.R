b70baf3ba643773bc72a77ba49a3701df2cf8764
warren yun <xxwarrenhdxx@gmail.com>
2021-06-23 10:19:02 -0400

    Create first question

--- script.R
+++ script.R
@@ -1,3 +1,12 @@
 rm(list=ls()) # get rid of all previously stored data
 
 heart_data <- read.csv("heart.csv", header = TRUE) # load data
+str(heart_data)
+
+
+# does gender have an effect on heart rate?
+male <- subset(heart_data, sex == 1)
+female <- subset(heart_data, sex == 0)
+
+mean(male$thalachh, na.rm = TRUE)
+mean(female$thalachh, na.rm = TRUE)
