b522e86d82d656a4208a65597340d7dbff28ef0c
warren yun <xxwarrenhdxx@gmail.com>
2021-06-23 23:51:12 -0400

    Add question on relationship between gender and likeliness of heart attack

--- script.R
+++ script.R
@@ -10,3 +10,7 @@ female <- subset(heart_data, sex == 0)
 
 mean(male$thalachh, na.rm = TRUE)
 mean(female$thalachh, na.rm = TRUE)
+
+# does gender have an effect on the likelyness of a heart attack
+mean(male$output, na.rm = TRUE)
+mean(female$output, na.rm = TRUE)
